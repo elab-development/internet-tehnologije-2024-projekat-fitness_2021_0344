53b5ff5f12a7a446c8d0d1a7d268c6d9c64a2174
maja303 <pavlovic.marija2002@gmail.com>
2025-09-29 20:12:59 +0200

    Napravljena je pocetna struktura projekta

---
+++ front_domaci/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+         
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
